5ff6e078238f133212ed97ae064fc67517f5c998
105599004 <gay88358@yahoo.com.tw>
2020-11-12 14:20:24 +0800

    refactor response format

--- src/backend/webserver/api/common/response.py
+++ src/backend/webserver/api/common/response.py
@@ -1,25 +1,26 @@
+def response_object(state, message, result):
+    return {
+            "state": state,
+            "message": message,
+            "result": result
+    }
 
 def error(result):
-    return {
-            "state": False,
-            "code": 400,
-            "message": result.error_messages,
-            "result": ""
-    }
+    return response_object(
+        False,
+        result.error_messages,
+        ""
+    )
 
 def success(result):
-    return {
-            "state": True,
-            "code": 200,
-            "message": "create success",
-            "result": result.value
-    }
+    return response_object(
+        True,
+        result.value,
+        "success"
+    )
     
 def response(result):
     if result.is_success():
         return success(result)
     else:
         return error(result)
-
-        
-    
